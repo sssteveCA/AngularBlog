fa467e5e05466ce0a53c5fbcef4bb45550fdde1f
sssteveCA <steppuggio@gmail.com>
2022-01-10 20:03:05 +0100

    content components added in content folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './content/index/index.component';
+import { BlogComponent } from './content/blog/blog.component';
+import { WhoWeAreComponent } from './content/who-we-are/who-we-are.component';
+import { NewsComponent } from './content/news/news.component';
+import { ContactsComponent } from './content/contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    IndexComponent,
+    BlogComponent,
+    WhoWeAreComponent,
+    NewsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
