baad09bbf755f03c699bb863edddc96745326a3d
sssteveCA <steppuggio@gmail.com>
2022-01-10 22:24:05 +0100

    Menu Routes added in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './content/blog/blog.component';
+import { ContactsComponent } from './content/contacts/contacts.component';
 import { IndexComponent } from './content/index/index.component';
+import { NewsComponent } from './content/news/news.component';
+import { WhoWeAreComponent } from './content/who-we-are/who-we-are.component';
 
-const routes: Routes = [{
-  path : "",
-  component : IndexComponent
-}];
+const routes: Routes = [
+  {
+    path : "",
+    component : IndexComponent
+  },
+  {
+    path: "blog",
+    component: BlogComponent
+  },
+  {
+    path: "chisiamo",
+    component: WhoWeAreComponent
+  },
+  {
+    path: "news",
+    component: NewsComponent
+  },
+  {
+    path: "contatti",
+    component: ContactsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
