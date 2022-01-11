febed81e50fc2b43de723a75359395c68f76e150
sssteveCA <steppuggio@gmail.com>
2022-01-11 22:36:53 +0100

    Added layout page in article.component.ts

--- src/app/content/article/article.component.ts
+++ src/app/content/article/article.component.ts
@@ -31,10 +31,57 @@ export class ArticleComponent implements OnInit {
        //console.log(res);
        let rJson = JSON.parse(res);
        console.log(rJson);
+       this.showArticle(rJson['article']);
      });
    }
 
   ngOnInit(): void {
+  }
+
+  //create HTML content from articles data
+  showArticle(data: any){
+    let divCont,divR,divC;
+    let title,content,categ,tags;
+    divCont = $('<div>');
+      divCont.addClass('container');
+        divR = $('<div>');
+        divR.addClass('row');
+          divC = $('<div>');
+          divC.addClass('col col-md-8 offset-md-2');
+            title = $('<h1>');
+            title.addClass('title');
+            title.text(data['title']);
+            title.css('text-align','center');
+          divC.append(title);
+        divR.append(divC);
+      divCont.append(divR);
+        divR = $('<div>');
+        divR.addClass('row');
+          divC = $('<div>');
+          divC.addClass('col col-md-8 offset-md-2 mt-3');
+            content = $('<div>');
+            content.text(data['content']);
+          divC.append(content);
+        divR.append(divC);
+      divCont.append(divR);
+       divR = $('<div>');
+       divR.addClass('row');
+        divC = $('<div>');
+        divC.addClass('col-12 col-md-5 mt-3');
+          categ = $('<p>');
+          categ.text("Categorie: "+data['categories']);
+          categ.css('font-weight','bold');
+        divC.append(categ);
+      divR.append(divC);
+       divC = $('<div>');
+       divC.addClass('col-12 col-md-5 offset-md-2 mt-3');
+        tags = $('<p>');
+        tags.text("Tag: "+data['tags']);
+        tags.css('font-weight','bold');
+       divC.append(tags);
+      divR.append(divC);
+    divCont.append(divR);
+    $('#article').append(divCont);
   }
 
 
