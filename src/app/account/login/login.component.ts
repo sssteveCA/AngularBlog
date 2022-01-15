229dfab17f7d710c8e26ff731f34669fd28934db
sssteveCA <steppuggio@gmail.com>
2022-01-15 19:08:22 +0100

    service for MySQL created

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder,private router: Router) { }
 
   ngOnInit(): void {
   }
---
+++ src/app/models/userblog.model.ts
@@ -0,0 +1,7 @@
+export class UserBlog{
+    id: number;
+    nome: string;
+    cognome: string;
+    username: string;
+    email: string;
+}
