5368f2a8783ae70d1dd7d65c4a98ce3f077e8f89
sssteveCA <steppuggio@gmail.com>
2022-05-29 00:41:56 +0200

    Updated articlelist.php
    Added articlelist_get method

--- src/assets/php/class/article/articlelist.php
+++ src/assets/php/class/article/articlelist.php
@@ -8,6 +8,22 @@ use AngularBlog\Classes\Article\Article;
 
 //This class is used to executed actions on multiple artciles at time
 class ArticleList extends Models implements Ale{
-    
+
+    private array $results = array(); //Array of Article objects result
+
+    public function getResults():array {return $this->results;}
+
+    public function articlelist_get(array $filter): bool{
+        $got = false;
+        $cursor = parent::get($filter);
+        if($this->errno == 0){
+            $results = $cursor->toArray();
+            foreach($results as $article){
+                //Add Article object from values gotten
+                $this->results[] = new Article($article);
+            }
+        }//if($this->errno == 0){
+        return $got;
+    }
 }
 ?>
