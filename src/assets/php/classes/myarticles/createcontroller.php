2dba41bac289651d7ddb4d0337b250ca12de04b2
sssteveCA <steppuggio@gmail.com>
2022-06-13 21:49:18 +0200

    Added src/assets/php/interfaces/myarticles/createcontroller_errors.php & createview_errors.php

--- src/assets/php/classes/myarticles/createcontroller.php
+++ src/assets/php/classes/myarticles/createcontroller.php
@@ -2,7 +2,29 @@
 
 namespace AngularBlog\Classes\Myarticles;
 
-class CreateController{
-    
+use AngularBlog\Interfaces\Constants as C;
+use AngularBlog\Classes\Token;
+use AngularBlog\Classes\Article\Article;
+
+class CreateController implements C{
+    private ?string $token_key;
+    private ?Token $token;
+    private ?Article $article;
+    private string $response = "";
+    private int $errno = 0;
+    private ?string $error = null;
+    private static string $logFile = C::FILE_LOG;
+
+    public function getTokenKey(){return $this->token_key;}
+    public function getArticle(){return $this->article;}
+    public function getResponse(){return $this->response;}
+    public function getErrno(){return $this->errno;}
+    public function getError(){
+        switch($this->errno){
+            default:
+                $this->error = null;
+        }
+        return $this->error;
+    }
 }
 ?>
