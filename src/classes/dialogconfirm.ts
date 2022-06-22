48795d799a3fa85c75d46f1dd5fd1a0d6ae4a4e2
sssteveCA <steppuggio@gmail.com>
2022-06-22 19:11:04 +0200

    Created dialogmessage.ts & dialogconfirm.ts in src/classes/ folder

---
+++ src/classes/dialogconfirm.ts
@@ -0,0 +1,69 @@
+//This class is to use the Bootstrap confirm dialog
+
+import * as bootstrap from "bootstrap";
+import ConfirmDialogInterface from "./confirmdialog.interface";
+
+export default class ConfirmDIalog{
+
+    private _bt_yes: Element; //'Yes' button
+    private _bt_no: Element; //'No' button
+    private _div_dialog: HTMLElement; //Dialog container
+    private _instance: bootstrap.Modal; //Bootstrap dialog instance
+    private _message: string; //Dialog message
+    private _title: string; //Dialog title
+    private _html: string; //HTML of bootstrap dialog
+
+    construct(dci: ConfirmDialogInterface){
+        this._title = dci.title;
+        this._message = dci.message;
+        this.htmlDialog();
+        this.showDialog();
+    }
+
+    get bt_yes(){return this._bt_yes;}
+    get bt_no(){return this._bt_no;}
+    get div_dialog(){return this._div_dialog;}
+    get instance(){return this._instance;}
+    get message(){return this._message;}
+    get title(){return this._title;}
+    get html(){return this._html;}
+
+    //Set html for the dialog
+    private htmlDialog(): string{
+        this._html = `
+<div id="dialog" class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">${this._title}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>${this._message}</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary dc_yesbutton">SÌ</button>
+                    <button type="button" class="btn btn-secondary dc_nobutton" data-bs-dismiss="modal">NO</button>
+                </div>
+            </div>
+        </div>
+</div>
+        `;
+        return this._html;
+    }
+
+    //Add dialog to DOM & show
+    private showDialog(): void{
+        this._div_dialog = document.createElement('div');
+        this._div_dialog.setAttribute('id','dc_div_dialog');
+        this._div_dialog.innerHTML = this._html;
+        document.body.appendChild(this._div_dialog);
+        let modalEl = document.getElementById('dialog');
+        this._instance = new bootstrap.Modal(modalEl as Element,{
+            focus: true
+        });
+        this._instance.show();
+        this._bt_yes = document.querySelector('.dc_yesbutton') as Element;
+        this._bt_no = document.querySelector('.dc_nobutton') as Element;
+    }
+}
