c8428573442a1c4bc5e287ea37ad9a90ab1b962d
sssteveCA <steppuggio@gmail.com>
2022-08-09 23:11:16 +0200

    Passed article property of Article component in Comments component as permalink property

--- src/app/content/article/comments/comments.component.ts
+++ src/app/content/article/comments/comments.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comments',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit {
 
+  @Input() permalink: string;
+
   constructor() { }
 
   ngOnInit(): void {
+
+
+
   }
 
 }
