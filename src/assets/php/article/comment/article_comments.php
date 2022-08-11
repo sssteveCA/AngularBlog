2a14be57ff1ad6954318d1f0cd9d65b65d0ce00b
sssteveCA <steppuggio@gmail.com>
2022-08-11 22:10:56 +0200

    Added comments_loop function in article_comments.php

--- src/assets/php/article/comment/article_comments.php
+++ src/assets/php/article/comment/article_comments.php
@@ -60,37 +60,7 @@ if(isset($_GET['permalink']) && $_GET['permalink'] != '' && $_GET['permalink'] !
             if($comments_found){
                 //At least one comment found
                 $comments = $cl->getResults();
-                $i = 0;
-                foreach($comments as $comment){
-                    $user = new User();
-                    $filter = [
-                        "_id" => new ObjectId($comment->getAuthor())
-                    ];
-                    $user_found = $user->user_get($filter);
-                    $response['comments'][$i] = [
-                        //'id' => $comment->getId(),
-                        //'article' => $comment->getArticle(),
-                        //'author' => $comment->getAuthor(),
-                        'author_name' => $user->getUsername(),
-                        'comment' => $comment->getComment(),
-                        'cu_comment' => false,
-                        'creation_time' => $comment->getCrTime(),
-                        'last_modified' => $comment->getLastMod()
-                    ];
-                    if($token !== null){
-                        //Add these properties if user is logged and it's his comment
-                        $comment_author_id = $comment->getAuthor();
-                        file_put_contents(C::FILE_LOG,"comment author id => ".var_export($comment_author_id,true)."\r\n",FILE_APPEND);
-                        $logged_user_id = $token->getUserId();
-                        file_put_contents(C::FILE_LOG,"logged user id => ".var_export($logged_user_id,true)."\r\n",FILE_APPEND);
-                        if($comment_author_id == $logged_user_id){
-                            //This comment belong to current logged user
-                            $response['comments'][$i]['id'] = $comment->getId();
-                            $response['comments'][$i]['cu_comment'] = true;
-                        }//if($comment_author_id == $logged_user_id){
-                    }//if($got_token !== null){
-                    $i++;
-                }//foreach($comments as $comment){
+                comments_loop($token,$comments,$response);
             }//if($comments_found){
             else{
                 $response['empty'] = true;
@@ -127,5 +97,40 @@ function token_exists(array $get): ?Token{
     }
     file_put_contents(C::FILE_LOG,"token_exists token => ".var_export($token,true)."\r\n",FILE_APPEND);
     return $token;
+}
+
+//Get the comments of the article and set them to response array
+function comments_loop(?Token $token, array $comments, array &$response){
+    $i = 0;
+    foreach($comments as $comment){
+        $user = new User();
+        $filter = [
+            "_id" => new ObjectId($comment->getAuthor())
+        ];
+        $user_found = $user->user_get($filter);
+        $response['comments'][$i] = [
+            //'id' => $comment->getId(),
+            //'article' => $comment->getArticle(),
+            //'author' => $comment->getAuthor(),
+            'author_name' => $user->getUsername(),
+            'comment' => $comment->getComment(),
+            'cu_comment' => false,
+            'creation_time' => $comment->getCrTime(),
+            'last_modified' => $comment->getLastMod()
+        ];
+        if($token !== null){
+            //Add these properties if user is logged and it's his comment
+            $comment_author_id = $comment->getAuthor();
+            file_put_contents(C::FILE_LOG,"comment author id => ".var_export($comment_author_id,true)."\r\n",FILE_APPEND);
+            $logged_user_id = $token->getUserId();
+            file_put_contents(C::FILE_LOG,"logged user id => ".var_export($logged_user_id,true)."\r\n",FILE_APPEND);
+            if($comment_author_id == $logged_user_id){
+                //This comment belong to current logged user
+                $response['comments'][$i]['id'] = $comment->getId();
+                $response['comments'][$i]['cu_comment'] = true;
+            }//if($comment_author_id == $logged_user_id){
+        }//if($got_token !== null){
+        $i++;
+    }//foreach($comments as $comment){
 }
 ?>
