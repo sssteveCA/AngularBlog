6178eaf204eba09cdb12cca584b9b54e18443719
sssteveCA <steppuggio@gmail.com>
2022-08-12 16:25:45 +0200

    Added getCommentByKey & isUsedAuthorizedCheck methods in comment commentauthorizedcontroller.php

--- src/assets/php/classes/article/comment/commentauthorizedcontroller.php
+++ src/assets/php/classes/article/comment/commentauthorizedcontroller.php
@@ -9,6 +9,8 @@ use AngularBlog\Interfaces\Article\Comment\CommentAuthorizedControllerErrors as 
 use AngularBlog\Traits\AuthorizedTrait;
 use AngularBlog\Traits\ResponseTrait;
 use AngularBlog\Interfaces\Constants as C;
+use MongoDB\BSON\ObjectId;
+use AngularBlog\Interfaces\TokenErrors as Te;
 
 //Determine if user is authorized to do operations with specificf comment
 class CommentAuthorizedController implements Cace{
@@ -23,7 +25,16 @@ class CommentAuthorizedController implements Cace{
         $this->checkVariables($data);
         $this->comment = $data['comment'];
         $this->token = $data['token'];
-
+        $tokenOk = $this->getTokenByKey();
+        if($tokenOk){
+            //Token exists
+            $commentOk = $this->getCommentById();
+            if($commentOk){
+                //Comment Exists
+                $authOk = $this->isUserAuthorizedCheck();
+            }
+        }//if($tokenOk){
+        $this->setResponse();
     }
 
     public function getComment(){return $this->comment;}
@@ -57,6 +68,21 @@ class CommentAuthorizedController implements Cace{
         if(!$data['token'] instanceof Token)throw new \Exception(Cace::TOKENTYPEMISMATCH_EXC);
     }
 
+    //Get article info by id
+    private function getCommentById(): bool{
+        $got = false;
+        $this->errno = 0;
+        $comment_id = $this->comment->getId();
+        $data = ['_id' => new ObjectId($comment_id)];
+        $comment_got = $this->comment->comment_get($data);
+        if($comment_got){
+            $got = true;
+        }
+        else
+            $this->errno = Cace::COMMENT_NOTFOUND;
+        return $got;
+    }
+
     //Get token by token key
     private function getTokenByKey(): bool{
         $got = false;
@@ -76,6 +102,51 @@ class CommentAuthorizedController implements Cace{
         else
             $this->errno = Cace::TOKEN_NOTFOUND;
         return $got;
+    }
+
+    //Check if user is authorized to edit this comment
+    private function isUserAuthorizedCheck(): bool{
+        $this->authorized = false;
+        $this->errno = 0;
+        $user_id = $this->token->getUserId();
+        $article_author = $this->comment->getAuthor();
+        if($user_id == $article_author){
+            //User is the owner of the article
+            $this->authorized = true;
+        }
+        else
+            $this->errno = Cace::FORBIDDEN;
+        return $this->authorized;
+    }
+
+    //Set the response to send to the view
+    private function setResponse(){
+        switch($this->errno){
+            case 0:
+                $this->response = "OK";
+                break;
+            case Cace::FROM_TOKEN:
+                $errnoT = $this->token->getErrno();
+                switch($errnoT){
+                    case Te::TOKENEXPIRED:
+                        $this->response = Te::TOKENEXPIRED_MSG;
+                        break;
+                    default:
+                        $this->response = C::ERROR_UNKNOWN;
+                        break;
+                }//switch($errnoT){
+                break;
+            case Cace::TOKEN_NOTFOUND:
+            case Cace::FORBIDDEN:
+                $this->response = Cace::FORBIDDEN_MSG;
+                break;
+            case Cace::COMMENT_NOTFOUND:
+                $this->response = Cace::COMMENT_NOTFOUND_MSG;
+                break;
+            default:
+                $this->response = C::ERROR_UNKNOWN;
+                break;
+        }//switch($this->errno){
     }
 }
 ?>
