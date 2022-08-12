5e1f7964915ca8102f9006bb084c746585e8e1bc
sssteveCA <steppuggio@gmail.com>
2022-08-12 16:33:43 +0200

    Added delete_comment method in comment deletecontroller.php

--- src/assets/php/classes/article/comment/deletecontroller.php
+++ src/assets/php/classes/article/comment/deletecontroller.php
@@ -8,6 +8,7 @@ use AngularBlog\Traits\ErrorTrait;
 use AngularBlog\Traits\ResponseTrait;
 use AngularBlog\Interfaces\Article\Comment\DeleteControllerErrors as Dce;
 USE AngularBlog\Classes\Article\Comment\CommentAuthorizedController;
+use MongoDB\BSON\ObjectId;
 
 class DeleteController implements Dce{
     use ErrorTrait, ResponseTrait;
@@ -21,6 +22,28 @@ class DeleteController implements Dce{
         $this->checkValues($data);
         $this->comment = $data['comment'];
         $this->token = $data['token'];
+        $auth = $this->checkAuthorization();
+        if($auth){
+            //User is authorized to delete this comment
+            $del = $this->delete_comment();
+        }
+    }
+
+    public function getComment(){return $this->comment;}
+    public function getToken(){return $this->token;}
+    public function getError(){
+        switch($this->errno){
+            case Dce::FROM_COMMENTAUTHORIZEDCONTROLLER:
+                $this->error = Dce::FROM_COMMENTAUTHORIZEDCONTROLLER_MSG;
+                break;
+            case Dce::COMMENTNOTDELETED:
+                $this->error = Dce::COMMENTNOTDELETED_MSG;
+                break;
+            default:
+                $this->error = null;
+                break;
+        }
+        return $this->error;
     }
 
     //Check if user is authorized to edit the article
@@ -47,6 +70,21 @@ class DeleteController implements Dce{
         if(!isset($data['token']))throw new \Exception(Dce::NOTOKENINSTANCE_EXC);
         if(!$data['comment'] instanceof Comment)throw new \Exception(Dce::INVALIDCOMMENTTYPE_EXC);
         if(!$data['token'] instanceof Token)throw new \Exception(Dce::INVALIDTOKENTYPE_EXC);
+    }
+
+    //Delete the comment
+    private function delete_comment(): bool{
+        $del = false;
+        $this->errno = 0;
+        $comment_id = $this->comment->getId();
+        $filter = ['_id' => new ObjectId($comment_id)];
+        $comment_delete = $this->comment->comment_delete($filter);
+        if($comment_delete)
+            $del = true;
+        else{
+            $this->errno = Dce::COMMENTNOTDELETED;
+        }
+        return $del;
     }
 
 
