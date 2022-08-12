d0978d0f9ecc3fa2f5b801d8470085ff705059a8
sssteveCA <steppuggio@gmail.com>
2022-08-12 20:33:38 +0200

    Added addComment & addCommentPromise methods in addcomment.ts

--- src/classes/requests/article/comment/addcomment.ts
+++ src/classes/requests/article/comment/addcomment.ts
@@ -1,13 +1,18 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Console } from "console";
+import { Messages } from "src/constants/messages";
 import AddCommentInterface from "src/interfaces/requests/article/comment/addcomment.interface";
 
 export default class AddComment{
     private _comment_text: string;
+    private _http: HttpClient;
     private _permalink: string;
     private _token_key: string;
     private _url: string;
 
-    constructor(data: AddCommentInterface){
+    constructor(data: AddCommentInterface, http: HttpClient){
         this._comment_text = data.comment_text;
+        this._http = data.http;
         this._permalink = data.permalink;
         this._token_key = data.token_key;
         this._url = data.url;
@@ -17,4 +22,41 @@ export default class AddComment{
     get permalink(){return this._permalink;}
     get token_key(){return this._token_key;}
     get url(){return this._url;}
+
+    public async addComment(): Promise<object>{
+        let response: object = {};
+        try{
+            const post_values: object = {
+                'comment_text': this._comment_text,
+                'permalink': this._permalink,
+                'token_key': this._token_key
+            };
+            this.addCommentPromise(post_values).then(res => {
+                console.log(res);
+                response = JSON.parse(res);
+            }).catch(err => {
+                throw err;
+            });
+        }catch(err){
+            response = {
+                'done': false,
+                'msg': Messages.COMMENTNEW_ERROR
+            };
+        }
+        return response;
+    }
+
+    private async addCommentPromise(createData: object): Promise<string>{
+        return await new Promise((resolve,reject)=>{
+            const headers: HttpHeaders = new HttpHeaders({
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+            });
+            this._http.post(this.url,createData,{headers: headers, responseType: 'text'}).subscribe(res => {
+              resolve(res);
+            }, error => {
+              reject(error);
+            });
+          });
+    }
 }
--- src/interfaces/requests/article/comment/addcomment.interface.ts
+++ src/interfaces/requests/article/comment/addcomment.interface.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from "@angular/common/http";
+
 export default interface AddCommentInterface{
     comment_text: string;
+    http: HttpClient;
     permalink: string;
     token_key: string;
     url: string;
