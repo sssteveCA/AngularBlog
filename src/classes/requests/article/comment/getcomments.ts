c0d1c91635ec9b541a10a68516276500d2679806
sssteveCA <steppuggio@gmail.com>
2022-08-12 22:22:22 +0200

    Added setFullUrl, getComments & getCommentsPromise methods in getcomments.ts

--- src/classes/requests/article/comment/getcomments.ts
+++ src/classes/requests/article/comment/getcomments.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from "@angular/common/http";
+import { Messages } from "src/constants/messages";
 import GetCommentsInterface from "src/interfaces/requests/article/comment/getcomments.interface";
 
 export default class GetComments{
@@ -16,11 +17,48 @@ export default class GetComments{
         else
             this._token_key = null;
         this._url = data.url;
+        this.setFullUrl();
     }
 
     get full_url(){return this._full_url;}
     get permalink(){return this._permalink;}
     get token_key(){return this._token_key;}
     get url(){return this._url;}
+
+    private setFullUrl(): void{
+        this._full_url = this._url+"?permalink="+this._permalink;
+        if(this._token_key)
+            this._full_url += "&token_key="+this._token_key;
+    }
+
+    public async getComments(): Promise<object>{
+        let response: object = {};
+        try{
+            await this.getCommentsPromise().then(res => {
+                console.log(res);
+                response = JSON.parse(res);
+                console.log(response);
+            }).catch(err => {
+                throw err;
+            });
+        }catch(err){
+            response = {
+                done: false,
+                msg: Messages.COMMENTLIST_ERROR
+            };
+        };
+        
+        return response;
+    }
+
+    private async getCommentsPromise(): Promise<string>{
+        return await new Promise<string>((resolve,reject)=>{
+            this._http.get(this._full_url,{responseType: 'text'}).subscribe(res =>{
+                resolve(res);
+            },error => {
+                reject(error);
+            });
+        });
+    }
 
 }
