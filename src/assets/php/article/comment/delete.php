a48b5e5219a0b33696f26c94516e354af49ddaa1
sssteveCA <steppuggio@gmail.com>
2022-08-12 19:59:22 +0200

    Fixed errors in delete comment in comment delete.php

--- src/assets/php/article/comment/delete.php
+++ src/assets/php/article/comment/delete.php
@@ -7,6 +7,8 @@ require_once("../../interfaces/token_errors.php");
 require_once("../../interfaces/article/comment/comment_errors.php");
 require_once("../../interfaces/article/comment/commentauthorizedcontroller_errors.php");
 require_once("../../interfaces/article/comment/commentauthorizedview_errors.php");
+require_once("../../interfaces/article/comment/deletecontroller_errors.php");
+require_once("../../interfaces/article/comment/deleteview_errors.php");
 require_once("../../vendor/autoload.php");
 require_once("../../traits/error.trait.php");
 require_once("../../traits/message.trait.php");
@@ -17,6 +19,8 @@ require_once("../../classes/token.php");
 require_once("../../classes/article/comment/comment.php");
 require_once("../../classes/article/comment/commentauthorizedcontroller.php");
 require_once("../../classes/article/comment/commentauthorizedview.php");
+require_once("../../classes/article/comment/deletecontroller.php");
+require_once("../../classes/article/comment/deleteview.php");
 
 use AngularBlog\Classes\Article\Comment\DeleteController;
 use AngularBlog\Classes\Article\Comment\DeleteView;
@@ -32,7 +36,7 @@ $response = [
     'done' => false,
     'expired' => false,
     'msg' => '',
-    'delete' => $delete
+    //'delete' => $delete
 ];
 
 if(isset($delete['token_key'],$delete['comment_id']) && $delete['token_key'] != '' && $delete['comment_id'] != ''){
