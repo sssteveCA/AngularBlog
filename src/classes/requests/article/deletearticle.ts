53ef6483d31637b3a797189f7e760360c1108056
sssteveCA <steppuggio@gmail.com>
2022-08-13 20:02:12 +0200

    Added deleteArticle & deleteArticlePromise methods in deletearticle.ts

--- src/classes/requests/article/deletearticle.ts
+++ src/classes/requests/article/deletearticle.ts
@@ -1,17 +1,55 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Messages } from "src/constants/messages";
 import DeleteArticleInterface from "src/interfaces/requests/article/deletearticle.interface";
 
 export default class DeleteArticle{
     private _article_id: string;
     private _http: HttpClient;
     private _token_key: string;
+    private _url: string;
 
     constructor(data: DeleteArticleInterface){
         this._article_id = data.article_id;
         this._http = data.http;
         this._token_key = data.token_key;
+        this._url = data.url;
     }
 
     get article_id(){return this._article_id;}
     get token_key(){return this._token_key;}
+    get url(){return this._url;}
+
+    public async deleteArticle(): Promise<object>{
+        let response: object = {};
+        try{
+            let deletearticle_values: object = {
+                article: this._article_id,
+                token_key: this._token_key
+            };
+            await this.deleteArticlePromise(deletearticle_values).then(res => {
+                console.log(res);
+                response = JSON.parse(res);
+                console.log(response);
+            }).catch(err => {
+                throw err;
+            });
+        }catch(err){
+            response = {
+                done: false,
+                msg: Messages.DELETEARTICLE_ERROR
+            };
+        }
+        return response;
+    }
+
+    private async deleteArticlePromise(deleteData: object): Promise<string>{
+        return await new Promise<string>((resolve,reject)=>{
+            const headers = new HttpHeaders().set('Content-Type','application/json').set('Accept','application/json');
+            this._http.post(this.url,deleteData,{headers: headers, responseType: 'text'}).subscribe(res => {
+                resolve(res);
+            },error => {
+                reject(error);
+            });
+        });
+    }
 }
--- src/interfaces/requests/article/deletearticle.interface.ts
+++ src/interfaces/requests/article/deletearticle.interface.ts
@@ -4,4 +4,5 @@ export default interface DeleteArticleInterface{
     article_id: string;
     http: HttpClient;
     token_key: string;
+    url: string;
 }
