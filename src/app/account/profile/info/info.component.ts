fc4b158f935746d5cdc11d820e8b37109f861c7f
Acer <Acer@DESKTOP-K09QF87>
2022-09-22 08:08:55 +0200

    Added observeFromService method in info.component.ts

--- src/app/account/profile/info/info.component.ts
+++ src/app/account/profile/info/info.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import * as constants from '../../../../constants/constants';
 
 @Component({
   selector: 'app-info',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoComponent implements OnInit {
 
-  constructor() { }
+  userCookie: any = {};
+
+  constructor(public http: HttpClient, public api: ApiService, public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  observeFromService(): void{
+    this.api.getLoginStatus().then(res => {
+      if(res == true){
+        this.userCookie['token_key'] = localStorage.getItem("token_key");
+        this.userCookie['username'] = localStorage.getItem("username");
+        this.api.changeUserdata(this.userCookie);
+      }//if(res == true){
+      else{
+        this.api.removeItems();
+        this.userCookie = {};
+        this.api.changeUserdata(this.userCookie);
+        this.router.navigateByUrl(constants.notLoggedRedirect);
+      }
+    }).catch(err => {
+      this.api.removeItems();
+        this.userCookie = {};
+        this.api.changeUserdata(this.userCookie);
+        this.router.navigateByUrl(constants.notLoggedRedirect);
+    });
+    this.api.loginChanged.subscribe(logged => {
+
+    });
+    this.api.userChanged.subscribe(userdata => {
+      this.userCookie['token_key'] = userdata['token_key'];
+      this.userCookie['username'] = userdata['username'];
+    });
   }
 
 }
