652b4f38b6398c1ecf5f95105ee84347629f363b
Acer <Acer@DESKTOP-K09QF87>
2022-10-11 18:47:39 +0200

    Added setResponse method in getusernamecontroller.php

--- src/assets/php/classes/account/info/getusernamecontroller.php
+++ src/assets/php/classes/account/info/getusernamecontroller.php
@@ -26,6 +26,22 @@ class GetUsernameController implements Guce {
         $this->checkValues($data);
         $this->token = $data['token'];
         $this->user = $data['user'];
+        $this->getUsername();
+        $this->setResponse();
+    }
+
+    public function getError(){
+        switch($this->errno){
+            case Guce::FROM_TOKEN:
+                $this->error = Guce::FROM_TOKEN_MSG;
+                break;
+            case Guce::FROM_USER:
+                $this->error = Guce::FROM_USER_MSG;
+                break;
+            default:
+                $this->error = null;
+                break;
+        }
     }
 
     private function checkValues(array $data){
@@ -39,12 +55,35 @@ class GetUsernameController implements Guce {
         $this->errno = 0;
         $token_key = $this->token->getTokenKey();
         $token_get = $this->token->token_get(['token_key' => $token_key]);
-        if($this->token->getErrno() != 0) return false;
+        if($this->token->getErrno() != 0){
+            $this->errno = Guce::FROM_TOKEN;
+            return false;
+        } 
         $user_id = $this->token->getUserId();
         $user_get = $this->user->user_get(['_id' => $user_id]);
-        if($this->user->getErrno() != 0)return false;
+        if($this->user->getErrno() != 0){
+            $this->errno = Guce::FROM_USER;
+            return false;
+        }
         $this->username = $this->user->getUsername();
         return true;
+    }
+
+    private function setResponse(){
+        switch($this->errno){
+            case 0:
+                $this->response = $this->username;
+                break;
+            case Guce::FROM_TOKEN:
+                $this->response = "";
+                break;
+            case Guce::FROM_USER:
+                $this->response = "";
+                break;
+            default:
+                $this->response = "";
+                break;
+        }
     }
 }
 ?>
--- src/assets/php/interfaces/account/info/getusernamecontroller_errors.php
+++ src/assets/php/interfaces/account/info/getusernamecontroller_errors.php
@@ -3,8 +3,9 @@
 namespace AngularBlog\Interfaces\Account\Info;
 
 use AngularBlog\Interfaces\ExceptionMessages;
+use AngularBlog\Interfaces\FromErrors;
 
-interface GetUsernameControllerErrors extends ExceptionMessages{
+interface GetUsernameControllerErrors extends ExceptionMessages, FromErrors{
 
 
 }
