89858e4b6838c832d1e8fad7e40df9858bb3bbe1
Acer <Acer@DESKTOP-K09QF87>
2022-10-16 17:20:02 +0200

    Added checkVariables method in userauthorizedcontroller.php

--- src/assets/php/classes/account/userauthorizedcontroller.php
+++ src/assets/php/classes/account/userauthorizedcontroller.php
@@ -4,14 +4,19 @@ namespace AngularBlog\Classes\Account;
 
 use AngularBlog\Classes\Token;
 use AngularBlog\Classes\User;
+use AngularBlog\Exceptions\NoTokenInstanceException;
+use AngularBlog\Exceptions\NoUserInstanceException;
+use AngularBlog\Exceptions\TokenTypeMismatchException;
+use AngularBlog\Exceptions\UserTypeMismatchException;
 use AngularBlog\Traits\AuthorizedTrait;
 use AngularBlog\Traits\ErrorTrait;
 use AngularBlog\Traits\ResponseTrait;
+use AngularBlog\Interfaces\Account\UserAuthorizedControllerErrors as Uace;
 
 /**
  * Check if user is authorized to manage the account
  */
-class UserAuthorizedController{
+class UserAuthorizedController implements Uace{
     use ErrorTrait, ResponseTrait, AuthorizedTrait;
 
     private ?Token $token;
@@ -19,7 +24,7 @@ class UserAuthorizedController{
 
     public function __construct(array $data)
     {
-        
+        $this->checkVariables($data);
     }
 
     public function getToken(){return $this->token;}
@@ -31,6 +36,13 @@ class UserAuthorizedController{
                 break;
         }
         return $this->error;
+    }
+
+    private function checkVariables(array $data){
+        if(!isset($data['token']))throw new NoTokenInstanceException(Uace::NOTOKENINSTANCE_EXC);
+        if(!isset($data['user']))throw new NoUserInstanceException(Uace::NOUSERINSTANCE_EXC);
+        if(!$data['token'] instanceof Token)throw new TokenTypeMismatchException(Uace::TOKENTYPEMISMATCH_EXC);
+        if(!$data['user'] instanceof User)throw new UserTypeMismatchException(Uace::USERTYPEMISMATCH_EXC);
     }
 }
 ?>
