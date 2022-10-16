09b6a2c7208300b1338c3fe62c583e621b5b1312
Acer <Acer@DESKTOP-K09QF87>
2022-10-16 17:15:50 +0200

    Added properties, constructor and getters in userauthorizedcontroller.php

--- src/assets/php/classes/account/userauthorizedcontroller.php
+++ src/assets/php/classes/account/userauthorizedcontroller.php
@@ -2,7 +2,35 @@
 
 namespace AngularBlog\Classes\Account;
 
+use AngularBlog\Classes\Token;
+use AngularBlog\Classes\User;
+use AngularBlog\Traits\AuthorizedTrait;
+use AngularBlog\Traits\ErrorTrait;
+use AngularBlog\Traits\ResponseTrait;
+
+/**
+ * Check if user is authorized to manage the account
+ */
 class UserAuthorizedController{
-    
+    use ErrorTrait, ResponseTrait, AuthorizedTrait;
+
+    private ?Token $token;
+    private ?User $user;
+
+    public function __construct(array $data)
+    {
+        
+    }
+
+    public function getToken(){return $this->token;}
+    public function getUser(){return $this->user;}
+    public function getError(){
+        switch($this->errno){
+            default:
+                $this->error = null;
+                break;
+        }
+        return $this->error;
+    }
 }
 ?>
