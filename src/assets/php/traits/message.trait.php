e0e8932c662d0d19b3aeef51b4c2ac6fd86550bd
Acer <Acer@DESKTOP-K09QF87>
2022-11-06 09:20:46 +0100

    added response code property to message.trait.php and response.trait.php

--- src/assets/php/traits/message.trait.php
+++ src/assets/php/traits/message.trait.php
@@ -4,9 +4,20 @@ namespace AngularBlog\Traits;
 
 //This trait is added to the Views classes for message and for checking nresult of the controller operations
 trait MessageTrait{
+    /**
+     * The HTTP response status code
+     */
+    private int $response_code;
+    /**
+     * The message that informs the success or failure of the request
+     */
     private string $message = "";
+    /**
+     * If true the request has been executed successfully, otherwise this variable is false
+     */
     private bool $done = false; //true if operations are done successfully
 
+    public function getResponseCode(){return $this->response_code;}
     public function getMessage(){return $this->message;}
     public function isDone(){return $this->done;}
 }
--- src/assets/php/traits/response.trait.php
+++ src/assets/php/traits/response.trait.php
@@ -3,8 +3,16 @@
 namespace AngularBlog\Traits;
 
 trait ResponseTrait{
+    /**
+     * The HTTP response status code
+     */
+    private string $response_code;
+    /**
+     * The response string message to send to the view
+     */
     private string $response = "";
 
+    public function getResponseCode(){return $this->response_code;}
     public function getResponse(){return $this->response;}
 }
 ?>
