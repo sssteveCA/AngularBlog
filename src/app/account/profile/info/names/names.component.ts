5d18ecada5a39f3b89d0f7359a65f0bf4bb02695
Acer <Acer@DESKTOP-K09QF87>
2022-11-15 22:34:30 +0100

    Added observeFromService and setFormGroupNames methods to names.component.ts

--- src/app/account/profile/info/names/names.component.ts
+++ src/app/account/profile/info/names/names.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-names',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NamesComponent implements OnInit {
 
-  constructor() { }
+  userCookie: any = {};
+  groupNames: FormGroup;
+  showNamesSpinner: boolean = false;
+  namesError: boolean = false;
+
+  constructor(public http: HttpClient, public api: ApiService, public router: Router, public fb: FormBuilder) { 
+    this.observeFromService();
+    this.setFormGroupNames();
+  }
 
   ngOnInit(): void {
+  }
+
+  private observeFromService(): void{
+    this.api.userChanged.subscribe(userdata => {
+      this.userCookie['token_key'] = userdata['token_key'];
+      this.userCookie['username'] = userdata['username'];
+    });
+  }
+
+  private setFormGroupNames(): void{
+    this.groupNames = this.fb.group({
+      'name': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      'surname': ['', Validators.compose([Validators.required,Validators.minLength(2)])]
+    });
   }
 
 }
