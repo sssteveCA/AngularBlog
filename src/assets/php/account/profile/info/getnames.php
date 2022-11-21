b3a1d38d564502e8856d52fd3540a96010c001f0
Acer <Acer@DESKTOP-K09QF87>
2022-11-21 20:32:11 +0100

    Added get name and surname operations to getnames.php

--- src/assets/php/account/profile/info/getnames.php
+++ src/assets/php/account/profile/info/getnames.php
@@ -1,10 +1,5 @@
 <?php
 
-use AngularBlog\Classes\Account\GetNamesController;
-use AngularBlog\Classes\Token;
-use AngularBlog\Classes\User;
-use Dotenv\Dotenv;
-
 require_once("../../../cors.php");
 require_once("../../../interfaces/constants.php");
 require_once("../../../interfaces/exception_messages.php");
@@ -13,8 +8,10 @@ require_once("../../../interfaces/model_errors.php");
 require_once("../../../interfaces/token_errors.php");
 require_once("../../../interfaces/user_errors.php");
 require_once("../../../interfaces/account/getnamescontroller_errors.php");
+require_once("../../../interfaces/account/getnamesview_errors.php");
 require_once("../../../traits/error.trait.php");
 require_once("../../../traits/message.trait.php");
+require_once("../../../traits/messagearray.trait.php");
 require_once("../../../traits/response.trait.php");
 require_once("../../../traits/responsemultiple.trait.php");
 require_once("../../../traits/error.trait.php");
@@ -22,9 +19,21 @@ require_once("../../../../../../vendor/autoload.php");
 require_once("../../../classes/model.php");
 require_once("../../../classes/token.php");
 require_once("../../../classes/user.php");
+require_once("../../../classes/account/getnamescontroller.php");
+require_once("../../../classes/account/getnamesview.php");
+
+use AngularBlog\Interfaces\TokenErrors as Te;
+use AngularBlog\Interfaces\Account\GetNamesControllerErrors as Gnce;
+use AngularBlog\Interfaces\Constants as C;
+use AngularBlog\Classes\Account\GetNamesController;
+use AngularBlog\Classes\Account\GetNamesView;
+use AngularBlog\Classes\Token;
+use AngularBlog\Classes\User;
+use Dotenv\Dotenv;
+
 
 $response = [
-    "done" => false, "expired" => false, "msg" => "", "name" => "", "surname" => ""
+    "done" => false, "expired" => false, "msg" => "", "data" => []
 ];
 
 if(isset($_GET["token_key"])){
@@ -36,10 +45,26 @@ if(isset($_GET["token_key"])){
         $user = new User([]);
         $gnc_data = [ 'token' => $token, 'user' => $user ];
         $gnc = new GetNamesController($gnc_data);
+        $gnv = new GetNamesView($gnc);
+        if($gnv->isDone()){
+            $response["done"] = true;
+            $data = $gnv->getMessageArray();
+            $response["data"]["name"] = $data["name"];
+            $response["data"]["surname"] = $data["surname"];
+        }//if($gnv->isDone()){
+        else{
+            if($guc->getErrno() == Gnce::FROM_TOKEN){
+                if($guc->getToken()->getErrno() == Te::TOKENEXPIRED)
+                    $response["expired"] = true;
+            }
+        }//else di if($gnv->isDone()){
+        http_response_code($gnv->getResponseCode());
     }catch(Exception $e){
         http_response_code(500);
+        $error = $e->getMessage();
+        file_put_contents(C::FILE_LOG, "{$error}\r\n",FILE_APPEND);
     }
-}
+}//if(isset($_GET["token_key"])){
 else
     $response["msg"] = "Fornisci un token di autorizzazione per continuare";
 
--- src/assets/php/classes/account/getnamesview.php
+++ src/assets/php/classes/account/getnamesview.php
@@ -4,6 +4,7 @@ namespace AngularBlog\Classes\Account;
 
 use AngularBlog\Interfaces\Account\GetNamesViewErrors as Gnve;
 use AngularBlog\Traits\MessageArrayTrait;
+use Exception;
 
 class GetNamesView implements Gnve{
 
@@ -13,8 +14,16 @@ class GetNamesView implements Gnve{
 
     public function __construct(GetNamesController $gnc)
     {
+        if(!$gnc) throw new Exception(Gnve::NOGETNAMESCONTROLLERINSTANCE_EXC);
         $this->gnc = $gnc;
-        if($this->gnc->getErrno() == 0){}
+        if($this->gnc->getErrno() == 0){
+            $this->done = true;
+            $this->message_array = $this->gnc->getResponseArray();
+        }
+        else{
+            $this->message = $this->gnc->getResponse();
+        }
+        $this->response_code = $this->gnc->getResponseCode();
     }
 }
 ?>
