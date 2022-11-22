a5a6a8fb671206619bf4546223a513b1e55c2d54
Acer <Acer@DESKTOP-K09QF87>
2022-11-22 11:46:47 +0100

    Added checkValues method to updatenamescontroller.php

--- src/assets/php/classes/account/updatenamescontroller.php
+++ src/assets/php/classes/account/updatenamescontroller.php
@@ -4,6 +4,10 @@ namespace AngularBlog\Classes\Account;
 
 use AngularBlog\Classes\Token;
 use AngularBlog\Classes\User;
+use AngularBlog\Exceptions\NoTokenInstanceException;
+use AngularBlog\Exceptions\NoUserInstanceException;
+use AngularBlog\Exceptions\TokenTypeMismatchException;
+use AngularBlog\Exceptions\UserTypeMismatchException;
 use AngularBlog\Interfaces\Account\UpdateNamesControllerErrors as Unce;
 use AngularBlog\Traits\ErrorTrait;
 use AngularBlog\Traits\ResponseTrait;
@@ -18,7 +22,7 @@ class UpdateNamesController implements Unce{
 
     public function __construct(array $data)
     {
-        
+       $this->checkValues($data); 
     }
 
     public function getToken(){return $this->token;}
@@ -39,5 +43,16 @@ class UpdateNamesController implements Unce{
         }
         return $this->error;
     }
+
+    private function checkValues(array $data){
+        if(!isset($data['token']))throw new NoTokenInstanceException(Unce::NOTOKENINSTANCE_EXC);
+        if(!isset($data['user']))throw new NoUserInstanceException(Unce::NOUSERINSTANCE_EXC);
+        if(!$data['token'] instanceof Token)throw new TokenTypeMismatchException(Unce::TOKENTYPEMISMATCH_EXC);
+        if(!$data['user'] instanceof User)throw new UserTypeMismatchException(Unce::USERTYPEMISMATCH_EXC);
+        $this->token = $data['token'];
+        $this->user = $data['user'];
+    }
+
+
 }
 ?>
