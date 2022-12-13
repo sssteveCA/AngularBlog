afd0802f3a3b14a25bb903be05acf833c96f9c52
Acer <Acer@DESKTOP-K09QF87>
2022-12-13 18:24:45 +0100

    Added properties, constructor and getters to deleteaccountcontroller.php

--- src/assets/php/classes/account/deleteaccountcontroller.php
+++ src/assets/php/classes/account/deleteaccountcontroller.php
@@ -2,11 +2,27 @@
 
 namespace AngularBlog\Classes\Account;
 
+use AngularBlog\Classes\Token;
+use AngularBlog\Classes\User;
 use AngularBlog\Interfaces\Account\DeleteAccountControllerErrors as Dace;
 use AngularBlog\Traits\ErrorTrait;
 use AngularBlog\Traits\ResponseTrait;
 
 class DeleteAccountController implements Dace{
     use ErrorTrait, ResponseTrait;
+
+    private string $conf_password;
+    private string $password;
+    private ?Token $token;
+    private ?User $user;
+    private ?User $uac_user;
+    private ?UserAuthorizedController $uac;
+
+    public function __construct(array $data){
+
+    }
+   
+    public function getToken(){return $this->token;}
+    public function getUser(){return $this->user;}
 }
 ?>
