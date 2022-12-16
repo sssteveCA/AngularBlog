30c5d2d3097ee7efb28a20f75dbb2fcac8c78ec5
stepp <stepp@mail.com>
2022-12-16 20:22:16 +0100

    Added src/assets/php/traits/emailmanager.trait.php

---
+++ src/assets/php/traits/emailmanager.trait.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AngularBlog\Traits;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+/**
+ * This trait contains some setter private methods of EmailManager class
+ */
+trait EmailManagerTrait{
+    private function assignValues(array $data){
+        $this->fromEmail = isset($data['from']) ? $data['from'] : null;
+        $this->toEmail = $data['to'];
+        $this->subject = $data['subject'];
+        $this->body = $data['body'];
+    }
+
+    /**
+     * Check if the needed values exist
+     */
+    private function checkExists(array $data): bool{
+        if(!isset($data['to']))return false;
+        if(!isset($data['subject']))return false;
+        if(!isset($data['body']))return false;
+        return true;
+    }
+
+    private function setEncoding(){
+        $this->CharSet = 'UTF-8';
+        $this->Encoding = 'base64';
+    }
+
+    private function setServerSettings(array $data){
+        //$this->SMTPDebug = SMTP::DEBUG_SERVER;
+        $this->SMTPDebug = SMTP::DEBUG_OFF;
+        $this->isSMTP();
+        $this->Host = isset($data['MAIL_HOST']) ? $data['MAIL_HOST'] : $_ENV['MAIL_HOST'];
+        $this->SMTPAuth = true;
+        $this->Username = isset($data['MAIL_USERNAME']) ? $data['MAIL_USERNAME'] : $_ENV['MAIL_USERNAME'];
+        $this->Password = isset($data['MAIL_PASSWORD']) ? $data['MAIL_PASSWORD'] : $_ENV['MAIL_PASSWORD'];
+        //$this->SMTPSecure = false;
+        $this->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $this->Port = isset($data['MAIL_PORT']) ? $data['MAIL_PORT'] : $_ENV['MAIL_PORT'];
+        $this->isHTML(true);
+    }
+}
+?>
