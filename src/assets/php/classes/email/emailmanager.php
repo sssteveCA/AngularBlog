04d99717dc511666a672698ea74d4ed4d0f87132
stepp <stepp@mail.com>
2022-12-16 20:19:17 +0100

    Added src/assets/php/classes/email/emailmanager.php

---
+++ src/assets/php/classes/email/emailmanager.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AngularBlog\Classes\Email;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class EmailManager extends PHPMailer{
+    /**
+     * Sender email
+     */
+    private ?string $fromEmail;
+
+    /**
+     * Receiver email
+     */
+    private string $toEmail;
+
+    /**
+     * The subject of the mail
+     */
+    private string $subject;
+
+    /**
+     * The content of the mail
+     */
+    private string $body;
+
+    public function __construct(array $data)
+    {
+        
+    }
+
+    public function getFromEmail(){ return $this->fromEmail; }
+    public function getToEmail(){ return $this->toEmail; }
+    public function getSubject(){ return $this->subject; }
+    public function getBody(){ return $this->body; }
+}
+?>
