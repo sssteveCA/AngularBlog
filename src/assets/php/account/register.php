4e2bcc0d3ac0daefbd4bbba5dd94db6987125662
stepp <stepp@mail.com>
2022-12-16 23:22:16 +0100

    Added password equality control to register.php

--- src/assets/php/account/register.php
+++ src/assets/php/account/register.php
@@ -34,33 +34,38 @@ $response['done'] = false;
 
 if(isset($post['name'],$post['surname'],$post['username'],$post['email'],$post['password'],$post['confPwd'],$post['subscribed'])){
     if(preg_match(User::$regex['password'],$post['password'])){
-        $dotenv = Dotenv::createImmutable(__DIR__."/../../../../");
-        $dotenv->safeLoad();
-        try{
-            $data = array(
-                'name' => $post['name'],
-                'surname' => $post['surname'],
-                'username' => $post['username'],
-                'email' => $post['email'],
-                'password' => $post['password'],
-                'subscribed' => $post['subscribed'],
-            );
-            $user = new User($data);
-            $rc = new RegistrationController($user);
-            $rv = new RegistrationView($rc);
-            if($rc->getErrno() == 0)$response['done'] = true;
-            $response['msg'] = $rv->getMessage();
-            http_response_code($rv->getResponseCode());
-        }
-        catch(Exception $e){
-            http_response_code(500);
-            file_put_contents(C::FILE_LOG,$e->getMessage()."\r\n",FILE_APPEND);
-            $response['msg'] = C::REG_ERROR;
+        if($post['password'] == $post['confPwd']){
+            $dotenv = Dotenv::createImmutable(__DIR__."/../../../../");
+            $dotenv->safeLoad();
+            try{
+                $data = array(
+                    'name' => $post['name'],
+                    'surname' => $post['surname'],
+                    'username' => $post['username'],
+                    'email' => $post['email'],
+                    'password' => $post['password'],
+                    'subscribed' => $post['subscribed'],
+                );
+                $user = new User($data);
+                $rc = new RegistrationController($user);
+                $rv = new RegistrationView($rc);
+                if($rc->getErrno() == 0)$response['done'] = true;
+                $response['msg'] = $rv->getMessage();
+                http_response_code($rv->getResponseCode());
+            }
+            catch(Exception $e){
+                http_response_code(500);
+                file_put_contents(C::FILE_LOG,$e->getMessage()."\r\n",FILE_APPEND);
+                $response['msg'] = C::REG_ERROR;
+            }
+        }//if($post['password'] == $post['confPwd']){
+        else{
+            $response['msg'] = C::ERROR_CONFIRM_PASSWORD_DIFFERENT;
         }
     }//if(preg_match(User::$regex['password'],$_POST['password'])){
     else{
         http_response_code(400);
-        $response['msg'] = C::ERROR_CONFIRM_PASSWORD_DIFFERENT;
+        $response['msg'] = "La password ha un formato non valido";
     }
         
 }//if(isset($_POST['name'],$_POST['surname'],$_POST['username'],$_POST['email'],$_POST['password'],$_POST['confPwd'],$_POST['subscribed'])){
