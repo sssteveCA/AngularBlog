cc4d41f02611104af06835d113f7e8f4a26410f3
stepp <stepp@mail.com>
2022-12-18 22:47:15 +0100

    Added showHideContent method to policy-item.component.ts

--- src/app/menu/policy-item/policy-item.component.ts
+++ src/app/menu/policy-item/policy-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HasElementRef } from '@angular/material/core/common-behaviors/color';
 
 @Component({
   selector: 'app-policy-item',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PolicyItemComponent implements OnInit {
 
+  @ViewChild('policyItem',{static: false})policyItemEl: ElementRef<HTMLLIElement>;
+
+  menuShown: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showHideContent(): void{
+    this.policyItemEl.nativeElement.classList.toggle('show');
+    let dropdownMenu: HTMLUListElement = this.policyItemEl.nativeElement.getElementsByClassName('dropdown-menu')[0] as HTMLUListElement;
+    dropdownMenu.classList.toggle('show');
+    if(!this.menuShown){
+      this.policyItemEl.nativeElement.setAttribute('aria-expanded','true');
+      dropdownMenu.setAttribute('data-bs-popper','static');
+    }
+    else{
+      this.policyItemEl.nativeElement.setAttribute('aria-expanded','false');
+      dropdownMenu.removeAttribute('data-bs-popper');
+    }
+    this.menuShown = !this.menuShown;
+  }
 }
