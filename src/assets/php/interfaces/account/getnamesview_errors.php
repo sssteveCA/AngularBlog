90cad02fc74f8a648815db0f0deee71a9909be99
stepp <stepp@mail.com>
2023-01-22 21:52:19 +0100

    Removed php files related to get name,surname and get username requests

--- src/assets/php/interfaces/account/getnamesview_errors.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AngularBlog\Interfaces\Account;
-
-interface GetNamesViewErrors{
-    //Exceptions
-    const NOGETNAMESCONTROLLERINSTANCE_EXC = "L'oggetto GetNamesController è uguale a null";
-}
-?>
--- src/assets/php/interfaces/account/getusernameview_errors.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AngularBlog\Interfaces\Account;
-
-interface GetUsernameViewErrors{
-    //Exceptions
-    const NOGETUSERNAMECONTROLLERINSTANCE_EXC = "L'oggetto GetUsernameController è uguale a null";
-}
-?>
