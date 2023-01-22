0ef6b544b01e9da537a548f60df39b72b3693da2
stepp <stepp@mail.com>
2023-01-22 12:40:35 +0100

    Added checkValues method to getuserinfocontroller.php

--- src/assets/php/classes/account/getuserinfocontroller.php
+++ src/assets/php/classes/account/getuserinfocontroller.php
@@ -4,6 +4,10 @@ namespace AngularBlog\Classes\Account;
 
 use AngularBlog\Classes\Token;
 use AngularBlog\Classes\User;
+use AngularBlog\Exceptions\NoTokenInstanceException;
+use AngularBlog\Exceptions\NoUserInstanceException;
+use AngularBlog\Exceptions\TokenTypeMismatchException;
+use AngularBlog\Exceptions\UserTypeMismatchException;
 use AngularBlog\Interfaces\Account\GetUserInfoControllerErrors as Guice;
 use AngularBlog\Traits\ErrorTrait;
 use AngularBlog\Traits\ResponseMultipleTrait;
@@ -31,6 +35,15 @@ class GetUserInfoController implements Guice{
     public function getName(){ return $this->name; }
     public function getSurname(){ return $this->surname; }
     public function getUsername(){ return $this->username; }
+
+    private function checkValues(array $data){
+        if(!isset($data['token'])) throw new NoTokenInstanceException(Guice::NOTOKENINSTANCE_EXC);
+        if(!isset($data['user'])) throw new NoUserInstanceException(Guice::NOUSERINSTANCE_EXC);
+        if(!$data['token'] instanceof Token) throw new TokenTypeMismatchException(Guice::TOKENTYPEMISMATCH_EXC);
+        if(!$data['user'] instanceof User) throw new UserTypeMismatchException(Guice::USERTYPEMISMATCH_EXC);
+        $this->token = $data['token'];
+        $this->user = $data['user'];
+    }
 
 }
 ?>
