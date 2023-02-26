64a1d7588740fce421162c033b84c3e73b86af7c
stepp <stepp@mail.com>
2023-02-26 11:34:39 +0100

    Added history and historyPromise methods to history.ts

--- src/classes/requests/profile/getuserinfo.ts
+++ src/classes/requests/profile/getuserinfo.ts
@@ -5,6 +5,12 @@ export default class GetUserInfo{
     private _http: HttpClient;
     private _token_key: string;
     private _url: string;
+    private _errno: number = 0;
+    private _error: string|null = null;
+
+    public static ERR_REQUEST: number = 1;
+
+    private static ERR_REQUEST_MSG: string = "Errore durante l'esecuzione della richiesta";
 
     constructor(data: GetUserInfoInterface){
         this._http = data.http;
@@ -12,18 +18,33 @@ export default class GetUserInfo{
         this._url = data.url;
     }
 
-    get token_key(){return this._token_key;}
     get url(){return this._url;}
+    get errno(){return this._errno;}
+    get error(){
+        switch(this._errno){
+            case GetUserInfo.ERR_REQUEST:
+                this._error = GetUserInfo.ERR_REQUEST_MSG;
+                break;
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
 
     public async getUserInfo(): Promise<object>{
         let response: object = {};
+        this._errno = 0;
         try{
             await this.getUserInfoPromise().then(res => {
                 //console.log(res);
                 response = JSON.parse(res);
+            }).catch(err => {
+                throw err;
             })
         }catch(err){
-
+            this._errno = GetUserInfo.ERR_REQUEST;
+            response = {done: false}
         }
         return response;
     }
--- src/classes/requests/profile/history.ts
+++ src/classes/requests/profile/history.ts
@@ -8,6 +8,10 @@ export default class History{
     private _errno: number = 0;
     private _error: string|null = null;
 
+    public static ERR_REQUEST: number = 1;
+
+    private static ERR_REQUEST_MSG: string = "Errore durante l'esecuzione della richiesta";
+
     constructor(data: HistoryInterface){
         this._http = data.http;
         this._token_key = data.token_key;
@@ -18,10 +22,42 @@ export default class History{
     get errno(){return this._errno;}
     get error(){
         switch(this._errno){
+            case History.ERR_REQUEST:
+                this._error = History.ERR_REQUEST_MSG;
+                break;
             default:
                 this._error = null;
                 break;
         }
         return this._error;
+    }
+
+    public async history(): Promise<object>{
+        let response: object = {};
+        this._errno = 0;
+        try{
+            await this.historyPromise().then(res => {
+                //console.log(res);
+                response = JSON.parse(res);
+            }).catch(err => {
+                throw err;
+            })
+        }catch(e){
+            this._errno = History.ERR_REQUEST;
+            response = {done: false}
+        }
+        return response;
+    }
+
+    private async historyPromise(): Promise<string>{
+        return await new Promise<string>((resolve,reject)=>{
+            this._http.get(`${this._url}?token_key=${this._token_key}`,{
+                responseType: 'text'
+            }).subscribe(res => {
+                resolve(res);
+            }, error => {
+                reject(error);
+            })
+        });
     }
 }
