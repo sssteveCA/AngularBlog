9b72674d643e7763294448f5f3f3ea94fc233ed5
stepp <stepp@mail.com>
2023-03-09 10:26:18 +0100

    Added history-item-style mixin to history-item.component.scss

--- src/app/account/profile/history/history-item/history-item.component.ts
+++ src/app/account/profile/history/history-item/history-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-history-item',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryItemComponent implements OnInit {
 
+  @Input() date: Date;
+  @Input() description: string;
+  @Input() title: string;
+  dateString: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.dateString = this.date.toLocaleString('it-It',{timeZone: 'UTC'});
   }
 
 }
