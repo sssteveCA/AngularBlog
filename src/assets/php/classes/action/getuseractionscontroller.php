987b5ffdff0b950d38d158bb6e42c3dd49ecd236
stepp <stepp@mail.com>
2023-03-16 21:51:45 +0100

    Added checkValues method to getuseractionscontroller.php

--- src/assets/php/classes/action/getuseractionscontroller.php
+++ src/assets/php/classes/action/getuseractionscontroller.php
@@ -4,10 +4,15 @@ namespace AngularBlog\Classes\Action;
 
 use AngularBlog\Classes\Token;
 use AngularBlog\Classes\User;
+use AngularBlog\Exceptions\NoTokenInstanceException;
+use AngularBlog\Exceptions\NoUserInstanceException;
+use AngularBlog\Exceptions\TokenTypeMismatchException;
+use AngularBlog\Exceptions\UserTypeMismatchException;
 use AngularBlog\Traits\ErrorTrait;
 use AngularBlog\Traits\ResponseMultipleTrait;
+use AngularBlog\Interfaces\Action\GetUserActionControllerErrors as Guace;
 
-class GetUserActionsController{
+class GetUserActionsController implements Guace{
 
     use ErrorTrait, ResponseMultipleTrait;
 
@@ -21,5 +26,14 @@ class GetUserActionsController{
 
     public function getToken(){ return $this->token; }
     public function getUser(){ return $this->user; }
+
+    private function checkValues(array $data){
+        if(!isset($data['token'])) throw new NoTokenInstanceException(Guace::NOTOKENINSTANCE_EXC);
+        if(!isset($data['user'])) throw new NoUserInstanceException(Guace::NOUSERINSTANCE_EXC);
+        if(!$data['token'] instanceof Token) throw new TokenTypeMismatchException(Guace::TOKENTYPEMISMATCH_EXC);
+        if(!$data['user'] instanceof User) throw new UserTypeMismatchException(Guace::USERTYPEMISMATCH_EXC);
+        $this->token = $data['token'];
+        $this->user = $data['user'];
+    }
 }
 ?>
