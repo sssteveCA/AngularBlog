cfdadb9a4f05cfbd652ca84e6e4fb74a7ec58f8e
stepp <stepp@mail.com>
2023-03-16 21:48:43 +0100

    Added src/assets/php/interfaces/action/getuseractionscontroller_errors.php

---
+++ src/assets/php/interfaces/action/getuseractionscontroller_errors.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AngularBlog\Interfaces\Action;
+
+use AngularBlog\Interfaces\ExceptionMessages;
+use AngularBlog\Interfaces\FromErrors;
+
+interface GetUserActionControllerErrors extends ExceptionMessages, FromErrors{
+
+}
+?>
