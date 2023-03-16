bd49972738577bebb16aae6e165ed70043982ac2
stepp <stepp@mail.com>
2023-03-16 22:13:00 +0100

    Added setUserActions method to getuseractionscontroller.php

--- src/assets/php/classes/action/getuseractionscontroller.php
+++ src/assets/php/classes/action/getuseractionscontroller.php
@@ -12,6 +12,7 @@ use AngularBlog\Exceptions\UserTypeMismatchException;
 use AngularBlog\Traits\ErrorTrait;
 use AngularBlog\Traits\ResponseMultipleTrait;
 use AngularBlog\Interfaces\Action\GetUserActionControllerErrors as Guace;
+use MongoDB\BSON\ObjectId;
 
 class GetUserActionsController implements Guace{
 
@@ -26,6 +27,9 @@ class GetUserActionsController implements Guace{
         if(!isset($data['token_key']))throw new \Exception(Guace::NOTOKENKEY_EXC);
         $this->token_key = $data['token_key'];
         $this->actionList = new ActionList();
+        if($this->setToken()){
+
+        }
     }
 
     public function getTokenKey(){ return $this->token_key; }
@@ -34,6 +38,9 @@ class GetUserActionsController implements Guace{
         switch($this->errno){
             case Guace::NOUSERIDFOUND:
                 $this->error = Guace::NOUSERIDFOUND_MSG;
+                break;
+            case Guace::NOACTIONFOUND:
+                $this->error = Guace::NOACTIONFOUND_MSG;
                 break;
             default:
                 $this->error = null;
@@ -46,12 +53,23 @@ class GetUserActionsController implements Guace{
      * Set the Token object
      */
     private function setToken(): bool{
-        $this->errno = 0;
         $this->token_key = new Token();
         $filter = ['token_key' => $this->token_key];
         $get = $this->token->token_get($filter);
         if($get) return true;
         $this->errno = Guace::NOUSERIDFOUND;
+        return false;
+    }
+
+    /**
+     * Get the actions performed by specific user
+     */
+    private function setUserActions(): bool{
+        $user_id = $this->token->getUserId();
+        $filter = ['user_id' => new ObjectId($user_id)];
+        $actionsGet = $this->actionList->actionlist_get($filter);
+        if($actionsGet) return true;
+        $this->errno = Guace::NOACTIONFOUND;
         return false;
     }
 }
--- src/assets/php/interfaces/action/getuseractionscontroller_errors.php
+++ src/assets/php/interfaces/action/getuseractionscontroller_errors.php
@@ -9,9 +9,11 @@ interface GetUserActionControllerErrors extends ExceptionMessages, FromErrors{
 
     //Numbers
     const NOUSERIDFOUND = 1;
+    const NOACTIONFOUND = 2;
 
      //Messages
      const NOUSERIDFOUND_MSG = "Nessun id utente con la chiave di login fornita";
+     const NOACTIONFOUND_MSG = "Non hai effettuato nessuna azione";
 
 }
 ?>
