58716b9526397729fc52cc8022680ef4309361ab
stepp <stepp@mail.com>
2023-04-17 20:26:16 +0200

    Added response data to deletehistoryitem.php

--- src/assets/php/account/profile/history/deletehistoryitem.php
+++ src/assets/php/account/profile/history/deletehistoryitem.php
@@ -1,14 +1,17 @@
 <?php
+use AngularBlog\Classes\Action\DeleteUserActionController;
+use AngularBlog\Classes\Action\DeleteUserActionView;
 
 require_once("../../../../../../vendor/autoload.php");
 
 use Dotenv\Dotenv;
 use AngularBlog\Interfaces\Constants as C;
+use AngularBlog\Interfaces\TokenErrors as Te;
 use AngularBlog\Classes\Token;
 use AngularBlog\Classes\Action\Action;
 
 $response = [
-    C::KEY_DONE => false, C::KEY_MESSAGE => ""
+    C::KEY_DONE => false, C::KEY_EXPIRED => false, C::KEY_MESSAGE => ""
 ];
 $headers = getallheaders();
 
@@ -20,10 +23,24 @@ if(isset($headers[C::KEY_AUTH],$_REQUEST['action_id']) && $headers[C::KEY_AUTH] 
     try{
         $token = new Token($token_data);
         $action = new Action($action_data);
+        $duac_data = [
+            'action' => $action, 'token' => $token
+        ];
+        $duac = new DeleteUserActionController($duac_data);
+        $duav = new DeleteUserActionView($duac);
+        $response[C::KEY_MESSAGE] = $duav->getMessage();
+        if($duav->isDone())
+            $response[C::KEY_DONE] = true;
+        else{
+            $errnoT = $duac->getToken()->getErrno();
+            if($errnoT == Te::TOKENEXPIRED){
+                $response[C::KEY_EXPIRED] = true;
+            }
+        }
+        http_response_code($duav->getResponseCode());
     }catch(Exception $e){
         http_response_code(500);
         $response[C::KEY_MESSAGE] = C::HISTORYITEM_DELETE_ERROR;
-
     }
 }//if(isset($headers[C::KEY_AUTH],$_REQUEST['action_id']) && $headers[C::KEY_AUTH] != "" && $_REQUEST['action_id'] != ""){
 else{
