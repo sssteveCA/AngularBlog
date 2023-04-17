a0b04e2e9a7732373a1a9a974739da52f01810fb
stepp <stepp@mail.com>
2023-04-17 18:21:16 +0200

    Added checkValues method to deleteuseractioncontroller.php

--- src/assets/php/classes/action/deleteuseractioncontroller.php
+++ src/assets/php/classes/action/deleteuseractioncontroller.php
@@ -3,8 +3,10 @@
 
 use AngularBlog\Classes\Action\Action;
 use AngularBlog\Classes\Token;
+use AngularBlog\Exceptions\ActionTypeMismatchException;
 use AngularBlog\Exceptions\NoActionInstanceException;
 use AngularBlog\Exceptions\NoTokenInstanceException;
+use AngularBlog\Exceptions\TokenTypeMismatchException;
 use AngularBlog\Interfaces\Action\DeleteUserActionControllerErrors as Duace;
 use AngularBlog\Traits\ErrorTrait;
 use AngularBlog\Traits\ResponseTrait;
@@ -18,7 +20,7 @@ class DeleteUserActionController implements Duace{
     private ?Action $aac_action;
 
     public function __construct(array $data){
-
+        $this->checkValues($data);
     }
 
     public function getToken(){return $this->token;}
@@ -36,7 +38,8 @@ class DeleteUserActionController implements Duace{
     private function checkValues(array $data){
         if(!isset($data['action']))throw new NoActionInstanceException(Duace::NOACTIONINSTANCE_EXC);
         if(!isset($data['token']))throw new NoTokenInstanceException(Duace::NOTOKENINSTANCE_EXC);
-        if(!$data['action'] instanceof Action){}
+        if(!$data['action'] instanceof Action) throw new ActionTypeMismatchException(Duace::INVALIDACTIONTYPE_EXC);
+        if(!$data['token'] instanceof Token)throw new TokenTypeMismatchException(Duace::INVALIDTOKENTYPE_EXC);
     }
 
 }
