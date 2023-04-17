d2cd1eac20cdc38d70021ffa8957c6ea5556f78f
stepp <stepp@mail.com>
2023-04-17 17:56:38 +0200

    Added src/assets/php/interfaces/action/deleteuseractioncontroller_errors.php

--- src/assets/php/account/profile/history/deletehistory.php
+++ src/assets/php/account/profile/history/deletehistory.php
@@ -4,6 +4,8 @@ require_once("../../../../../../vendor/autoload.php");
 
 use Dotenv\Dotenv;
 use AngularBlog\Interfaces\Constants as C;
+use AngularBlog\Classes\Token;
+use AngularBlog\Classes\Action\Action;
 
 $response = [
     C::KEY_DONE => false, C::KEY_MESSAGE => ""
@@ -13,8 +15,11 @@ $headers = getallheaders();
 if(isset($headers[C::KEY_AUTH],$_REQUEST['action_id']) && $headers[C::KEY_AUTH] != "" && $_REQUEST['action_id'] != ""){
     $dotenv = Dotenv::createImmutable(__DIR__."/../../../../../../");
     $dotenv->safeLoad();
+    $token_data = ['token_key' => $headers[C::KEY_AUTH]];
+    $action_data = ['id' => $_REQUEST['action_id']];
     try{
-
+        $token = new Token($token_data);
+        $action = new Action($action_data);
     }catch(Exception $e){
         http_response_code(500);
         $response[C::KEY_MESSAGE] = C::HISTORYITEM_DELETE_ERROR;
---
+++ src/assets/php/interfaces/action/deleteuseractioncontroller_errors.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AngularBlog\Interfaces\Action;
+use AngularBlog\Interfaces\ExceptionMessages;
+use AngularBlog\Interfaces\FromErrors;
+
+interface DeleteUserActionErrors extends ExceptionMessages, FromErrors{
+    
+}
+?>
