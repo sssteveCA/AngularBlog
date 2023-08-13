2c313bf66f8f1e1584fb17566f9886d8ea724365
stepp <stepp@mail.com>
2023-08-13 18:01:20 +0200

    Added loginStatusRequest and loginStatusRequestPromise method to logindata.service.ts

--- src/app/services/logindata.service.ts
+++ src/app/services/logindata.service.ts
@@ -1,17 +1,54 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { UserCookie } from 'src/constants/types';
+import { LoginStatusResponse } from 'src/constants/types';
+import * as constants from '../../constants/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogindataService {
 
-  private userCookie = new Subject<UserCookie>();
+  private userCookie = new Subject<LoginStatusResponse>();
 
   constructor(private http: HttpClient) {
 
+  }
+
+  /**
+   * Check if user is still logged
+   */
+  private async loginStatusRequest(): Promise<boolean>{
+    let logged = false;
+    const token_key = localStorage.getItem('token_key');
+    const username = localStorage.getItem('username');
+    if(token_key && username){
+      //Token_key exists
+      const headers = new HttpHeaders({'Accept': 'application/json','Content-Type': 'application/json'});
+      const body: object = {
+        'token_key': token_key, 
+        'username': username
+      };
+      await this.loginStatusRequestPromise(constants.loginStatusUrl,body,headers).then(res => {
+        let rJson = JSON.parse(res);
+        if(rJson['logged'] == true){
+          logged = true;
+        }
+      }).catch(err => {});
+    }//if(token_key && username){
+    return logged;
+  }
+
+  private async loginStatusRequestPromise(url: string, body: object, headers: HttpHeaders): Promise<string>{
+    let promise = await new Promise<string>((resolve,reject) => {
+      this.http.post(url,body,{headers: headers, responseType: 'text'}).subscribe({
+        next: (res) => resolve(res),
+        error: (error) => {
+          reject(error);
+        }
+      })
+    });
+    return promise;
   }
 
 
