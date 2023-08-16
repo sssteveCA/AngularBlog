15d8ff24c840f49351020c06e293e7a2a115306d
stepp <stepp@mail.com>
2023-08-16 12:04:30 +0200

    Added loginDataObserver method to profile component

--- src/app/account/profile/profile.component.ts
+++ src/app/account/profile/profile.component.ts
@@ -1,14 +1,17 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscribable, Subscription } from 'rxjs';
+import { LogindataService } from 'src/app/services/logindata.service';
 import { UserCookie } from 'src/constants/types';
+import * as constants from '../../../constants/constants';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
   cookie: UserCookie = {};
   menuItems: object[] = [
@@ -17,14 +20,33 @@ export class ProfileComponent implements OnInit {
     {title: "Crea un nuovo articolo", text: "Crea e pubblica un nuovo articolo", link: "myArticles/create"},
     {title: "Cronologia azioni effettuate", text: "Visualizza o rimuovi tutte le azioni effettuate con questo account", link: "history"}
   ]
+  subscription: Subscription;
 
-  constructor(private http:HttpClient, private router: Router) {
+  constructor(private http:HttpClient, private router: Router, private loginData: LogindataService) {
    }
+
+
+  ngOnDestroy(): void {
+    if(this.subscription != null) this.subscription.unsubscribe();
+  }
 
   ngOnInit(): void {
      this.cookie = {
       token_key: localStorage.getItem('token_key'), username: localStorage.getItem('username')
     }
+  }
+
+
+  loginDataObserver(): void{
+    this.subscription = this.loginData.userCookieObservable.subscribe(userCookie => {
+      if(userCookie && 'token_key' in userCookie && 'username' in userCookie){
+        this.cookie.username = userCookie.username;
+        this.cookie.token_key = userCookie.token_key;
+      }
+      else{
+        this.router.navigateByUrl(constants.homeUrl);
+      }
+    })
   }
 
 }
