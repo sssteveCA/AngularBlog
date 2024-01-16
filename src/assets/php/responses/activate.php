45b755083cbac0260692a0a3c7ba759b72fc544f
= <=>
2024-01-16 07:49:16 +0000

    Added activate.php content method

--- src/assets/php/responses/activate.php
+++ src/assets/php/responses/activate.php
@@ -14,7 +14,47 @@ use Dotenv\Dotenv;
 class Activate{
 
     public static function content(array $params): array{
-
+        $response = [
+            C::KEY_CODE => 200, C::KEY_DONE => false, C::KEY_MESSAGE => ''
+        ];
+        $get = $params['get'];
+        if(isset($get['emailVerif']) && $get['emailVerif'] != ''){
+            if(preg_match(User::$regex['emailVerif'],$_REQUEST['emailVerif'])){
+                try{
+                    $dotenv = Dotenv::createImmutable(__DIR__."/../../../../");
+                    $dotenv->load();
+                    $emailVerif = $get['emailVerif'];
+                    $data = array('emailVerif' => $emailVerif);
+                    $user = new User($data);
+                    $vc = new VerifyController($user);
+                    $vv = new VerifyView($vc);
+                    $response[C::KEY_MESSAGE] = $vv->getMessage();
+                    switch($response[C::KEY_MESSAGE]){
+                        case C::ACTIVATION_OK:
+                            $response[C::KEY_CODE] = 200;
+                            $response['status'] = 1;
+                            break;
+                        case C::ACTIVATION_INVALID_CODE:
+                            $response[C::KEY_CODE] = 400;
+                            $response['status'] = -1;
+                            break;
+                        default:
+                            $response[C::KEY_CODE] = 500;
+                            $response['status'] = -2;
+                            break;
+                    }
+                }catch(Exception $e){
+                    $response[C::KEY_CODE] = 500;
+                    $response['status'] = -2;
+                }
+            }
+        }
+        else{
+            $response[C::KEY_CODE] = 400;
+            $response['status'] = 0;
+        }
+        return $response;
+        
     }
 }
 
