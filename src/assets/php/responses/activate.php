50f6a89c2934bfbe58c91521e7cffcaa3b992fa1
= <=>
2024-01-16 07:43:18 +0000

    Added activate.php

---
+++ src/assets/php/responses/activate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AngularBlog\Responses;
+
+use AngularBlog\Interfaces\Constants as C;
+use AngularBlog\Classes\User;
+use AngularBlog\Classes\Subscribe\VerifyController;
+use AngularBlog\Classes\Subscribe\VerifyView;
+use Dotenv\Dotenv;
+
+/**
+ * JSON response for account activation GET route
+ */
+class Activate{
+
+    public static function content(array $params): array{
+
+    }
+}
+
+?>
