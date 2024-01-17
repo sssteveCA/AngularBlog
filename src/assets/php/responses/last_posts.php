ec9290dcda7e8fd761a802272a83c627ee02342f
= <=>
2024-01-17 22:52:25 +0000

    Updated responses last_posts.php content method

--- src/assets/php/responses/last_posts.php
+++ src/assets/php/responses/last_posts.php
@@ -2,7 +2,12 @@
 
 namespace AngularBlog\Responses;
 
+use AngularBlog\Classes\Article\ArticleList;
+use AngularBlog\Classes\User;
 use AngularBlog\Interfaces\Constants as C;
+use Dotenv\Dotenv;
+use MongoDB\BSON\ObjectId;
+use Exception;
 
 /**
  * JSON response for last post GET route
@@ -11,8 +16,46 @@ class LastPosts{
 
     public function content(array $params): array{
         $response = [
-            C::KEY_DATA => [], C::KEY_DONE => false, C::KEY_EMPTY => false, C::KEY_MESSAGE => ""
+            C::KEY_CODE => 200, C::KEY_DATA => [], C::KEY_DONE => false, C::KEY_EMPTY => false, C::KEY_MESSAGE => ""
         ];
+        try{
+            $dotenv = Dotenv::createImmutable(__DIR__."/../../../../");
+            $dotenv->load();
+            $al = new ArticleList();
+            $options = [
+                'limit' => 10,
+                'sort' => ['last_modified' => -1]
+            ];
+            $found = $al->articlelist_get([],$options);
+            if($found){
+                $articles = $al->getResults();
+                $i = 0;
+                foreach($articles as $article){
+                    $response[C::KEY_DATA][$i] = array(
+                        'categories' => implode(",", $article->getCategories()),
+                        'introtext' => $article->getIntrotext(),
+                        'last_modified' => $article->getLastMod(),
+                        'permalink' => $article->getPermalink(),
+                        'tags' => implode(",", $article->getTags()),
+                        'title' => $article->getTitle(),
+                    );
+                    $user = new User([]);
+                    $got = $user->user_get(['_id' => new ObjectId($article->getAuthor())]);
+                    if($got) $author = $user->getUsername();
+                    else $author = "Autore sconosciuto";
+                    $response[C::KEY_DATA][$i]['author'] = $author;
+                    $i++;
+                }
+            }
+            else{
+                $response[C::KEY_EMPTY] = true;
+                $response[C::KEY_MESSAGE] = C::NEWS_EMPTY;
+            }
+            $response[C::KEY_DONE] = true;
+        }catch(Exception $e){
+            $response[C::KEY_MESSAGE] = C::NEWS_ERROR;
+            $response[C::KEY_CODE] = 500;
+        }
         return $response;
     }
 }
