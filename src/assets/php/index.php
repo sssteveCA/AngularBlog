e9dbc6489f680518afcb16ce918d2dd9ef9dc21a
= <=>
2024-01-19 22:05:19 +0000

    Added '/api/v1/articles/<permalink>' route

--- src/assets/php/index.php
+++ src/assets/php/index.php
@@ -4,6 +4,7 @@ require_once('interfaces/article/articlelist_errors.php');
 require_once('../../../vendor/autoload.php');
 
 use AngularBlog\Responses\Activate;
+use AngularBlog\Responses\GetArticle;
 use AngularBlog\Responses\LastPosts;
 use AngularBlog\Responses\Login;
 use AngularBlog\Responses\Logout;
@@ -19,8 +20,14 @@ $prefix = "/api/v1";
 if($method == "GET"){
     $prefixSlashes = str_replace('/','\/',$prefix);
     if(preg_match("/^{$prefixSlashes}\/activate\/([0-9a-zA-Z]{64})/",$uri,$matches)){
-        $params = [ 'get' => ['emailVerif' => $matches[1]]];
+        $params = [ 'get' => [ 'emailVerif' => $matches[1] ] ];
         $response = Activate::content($params);
+        http_response_code($response[C::KEY_CODE]);
+        echo json_encode($response,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+    }
+    else if(preg_match("/^{$prefixSlashes}\/articles\/([a-zA-Z\d\-_]{5,30})/",$uri,$matches)){
+        $params = [ 'get' => [ 'permalink' => $matches[1] ] ];
+        $response = GetArticle::content($params);
         http_response_code($response[C::KEY_CODE]);
         echo json_encode($response,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
     }
