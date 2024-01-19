3c7de32ca808da9d1d2680d5d8547caa642e73be
= <=>
2024-01-19 22:34:57 +0000

    Updated GetArticlesByQuery content method

--- src/assets/php/responses/getarticlesbyquery.php
+++ src/assets/php/responses/getarticlesbyquery.php
@@ -2,6 +2,12 @@
 
 namespace AngularBlog\Responses;
 
+use AngularBlog\Interfaces\Constants as C;
+use AngularBlog\Classes\Article\ArticleList;
+use Dotenv\Dotenv;
+use MongoDB\BSON\Regex;
+use Exception;
+
 /**
  * JSON response for get articles list by query GET route
  */
@@ -11,6 +17,43 @@ class GetArticlesByQuery{
         $response = [
             C::KEY_CODE => 200, C::KEY_DONE => false, C::KEY_MESSAGE => ''
         ];
+        $get = $params['get'];
+        $field = 'title';
+        if(isset($get['query']) && $get['query'] != ''){
+            try{
+                $dotenv = Dotenv::createImmutable(__DIR__."/../../../../");
+                $dotenv->load();
+                $query = $get['query'];
+                $al = new ArticleList();
+                $regex = new Regex($query,'i');
+                $filter = [ 'title' =>  $regex ];
+                $found = $al->articlelist_get($filter);
+                if($found){
+                    //At least one article found
+                    $articles = $al->getResults();
+                    foreach($articles as $article){
+                        $response[C::KEY_DATA][] = array(
+                            'title' => $article->getTitle(),
+                            'author' => $article->getAuthor(),
+                            'permalink' => $article->getPermalink(),
+                            'introtext' => $article->getIntrotext(),
+                        );
+                    }//foreach($articles as $article){
+                    $response[C::KEY_DONE] = true;
+                }
+                else{
+                    $response[C::KEY_EMPTY] = true;
+                    $response[C::KEY_MESSAGE] = 'La ricerca di '.$query.' non ha fornito alcun risultato';
+                }
+            }catch(Exception $e){
+                $response[C::KEY_CODE] = 500;
+                $response[C::KEY_MESSAGE] = C::SEARCH_ERROR;
+            }
+        }
+        else{
+            $response[C::KEY_CODE] = 400;
+            $response[C::KEY_MESSAGE] = C::FILL_ALL_FIELDS;
+        }
         return $response;
     }
 }
