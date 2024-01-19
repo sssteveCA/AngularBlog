7c6ada5e99599c0fe2ed6914b1389005ebc344ec
= <=>
2024-01-19 21:38:59 +0000

    Added getarticle.php

---
+++ src/assets/php/responses/getarticle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AngularBlog\Responses;
+
+/**
+ * JSON response for single article GET route
+ */
+class GetArticle{
+
+    public static function content(array $params): array{
+        
+    }
+
+}
+
+?>
