b789031e05141e1177d093273b28a1ffb83b4ebd
= <=>
2024-01-20 23:39:35 +0000

    Updated responses getuserarticles.php content method

--- src/assets/php/responses/getuserarticles.php
+++ src/assets/php/responses/getuserarticles.php
@@ -18,6 +18,50 @@ class GetUserArticles{
         $response = [
             C::KEY_CODE => 200, C::KEY_DONE => false, C::KEY_EMPTY => false, C::KEY_MESSAGE => '', 'articles' => []
         ];
+        $headers = $params['headers'];
+        if(isset($headers[C::KEY_AUTH]) && $headers[C::KEY_AUTH] != ''){
+            try{
+                $dotenv = Dotenv::createImmutable(__DIR__."/../../../../");
+                $dotenv->load();
+                $data = [ 'token_key' => $headers[C::KEY_AUTH] ];
+                $getController = new GetController($data);
+                $getView = new GetView($getController);
+                if($getView->articlesFound()){
+                    //At least one article found
+                    $articles = $getController->getArticleList()->getResults();
+                    foreach($articles as $article){
+                        $response['articles'][] = array(
+                            'id' => $article->getId(),
+                            'title' => $article->getTitle(),
+                            'author' => $article->getAuthor(),
+                            'permalink' => $article->getPermalink(),
+                            'content' => $article->getContent(),
+                            'introtext' => $article->getIntrotext(),
+                            'categories' => implode(",",$article->getCategories()),
+                            'tags' => implode(",",$article->getTags()),
+                            'creation_time' => $article->getCrTime(),
+                            'last_modified' => $article->getLastMod()
+                        );
+                    }//foreach($articles as $article){
+                    $response[C::KEY_DONE] = true;
+                }
+                else{
+                    $response[C::KEY_MESSAGE] = $getView->getMessage();
+                    if($response[C::KEY_MESSAGE] == Gce::NOARTICLESFOUND_MSG) {
+                        $response[C::KEY_EMPTY] = true;
+                        $response[C::KEY_DONE] = true;
+                    }
+                }
+                $response[C::KEY_CODE] = $getView->getResponseCode();
+            }catch(Exception $e){
+                $response[C::KEY_CODE] = 500;
+                $response[C::KEY_MESSAGE] = C::SEARCH_ERROR;
+            }
+        }
+        else{
+            $response[C::KEY_CODE] = 400;
+            $response[C::KEY_MESSAGE] = C::FILL_ALL_FIELDS;
+        }
         return $response;
     }
 }
