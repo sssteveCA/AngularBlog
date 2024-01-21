60fbe3e61f44ce1c44b1470db4686e9652c45157
= <=>
2024-01-21 00:04:06 +0000

    Updated routes

--- src/assets/php/index.php
+++ src/assets/php/index.php
@@ -28,14 +28,10 @@ $regex = [
     "permalink" => "([a-zA-Z\d\-_]{5,30})",
     "token" => "([0-9a-zA-Z]{64})"
 ];
-$objectIdRegex = "([0-9a-fA-F]{24})";
-$prefixSlashes = str_replace('/','\/',$prefix);
-$permalinkRegex = "([a-zA-Z\d\-_]{5,30})";
-$tokenRegex = "([0-9a-zA-Z]{64})";
 
 if($method == "GET"){
     $params = [ 'get' => [], 'headers' => $headers ];
-    if(preg_match("/^{$prefixSlashes}\/activate\/{$regex['token']}/",$uri,$matches)){
+    if(preg_match("/^{$prefixSlashes}\/activate\/{$regex['token']}\/?$/",$uri,$matches)){
         $params['get']['emailVerif'] = $matches[1];
         $response = Activate::content($params);
     }
@@ -43,21 +39,21 @@ if($method == "GET"){
         $params['get']['permalink'] = $matches[1];
         $response = GetArticle::content($params);
     }
-    else if(preg_match("/^{$prefixSlashes}\/articles\/{$regex['permalink']}\/comments/",$uri,$matches)){
+    else if(preg_match("/^{$prefixSlashes}\/articles\/{$regex['permalink']}\/comments\/?$/",$uri,$matches)){
         $params['get']['permalink'] = $matches[1];
         $response = ArticleComments::content($params);
     }
-    else if(substr($uri,0,strlen($prefix."/articles")) == $prefix."/articles" && isset($_GET['query'])){
-        $params['get']['query'] = $_GET['query'];
+    else if(preg_match("/^{$prefixSlashes}\/articles\?query=([a-zA-z0-9]{3,30})$/",$uri,$matches)){
+        $params['get']['query'] = $matches[1];
         $response = GetArticlesByQuery::content($params);
     }
-    else if($uri == $prefix."/lastposts"){
+    else if(preg_match("/^{$prefixSlashes}\/lastposts\/?$/",$uri)){
         $response = LastPosts::content([]);
     }
-    else if($uri == $prefix."/logout"){
+    else if(preg_match("/^{$prefixSlashes}\/logout\/?$/",$uri)){
         $response = Logout::content($params);
     }
-    else if($uri == $prefix."/profile/articles"){
+    else if(preg_match("/^{$prefixSlashes}\/profile\/articles\/?$/",$uri)){
         $response = GetUserArticles::content($params);
     }
 }
@@ -65,13 +61,13 @@ else if($method == "POST"){
     $input = file_get_contents("php://input");
     $post = json_decode($input, true);
     $params = [ 'headers' => $headers, 'post' => $post ];
-    if($uri == $prefix."/articles"){
+    if(preg_match("/^{$prefixSlashes}\/articles\/?$/",$uri)){
         $response = CreateArticle::content($params);
     }
-    else if($uri == $prefix."/login"){
+    else if(preg_match("/^{$prefixSlashes}\/login\/?$/",$uri)){
         $response = Login::content($params);
     }
-    else if($uri == $prefix."/register"){
+    else if(preg_match("/^{$prefixSlashes}\/register\/?$/",$uri)){
         $response = Register::content($params);
     }
 }
@@ -80,7 +76,7 @@ else if($method == "PUT"){
     $input = file_get_contents("php://input");
     $put = json_decode($input, true);
     $params = [ 'headers' => $headers, 'put' => $put ];
-    if(preg_match("/^{$prefixSlashes}\/articles\/{$regex['objectId']}/",$uri,$matches)){
+    if(preg_match("/^{$prefixSlashes}\/articles\/{$regex['objectId']}\/?$/",$uri,$matches)){
         $params['put']['article']['id'] = $matches[1];
         $response = EditArticle::content($params);
     }
