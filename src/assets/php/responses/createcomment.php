41d25454a895cd647f0d9b3bdff131fb0032a8e8
= <=>
2024-01-21 17:32:19 +0000

    Updated responses createcomment.php content method

--- src/assets/php/responses/createcomment.php
+++ src/assets/php/responses/createcomment.php
@@ -21,9 +21,40 @@ class CreateComment{
         ];
         $headers = $params['headers'];
         $post = $params['post'];
+        if(isset($post['permalink'],$headers[C::KEY_AUTH],$post['comment_text']) && $post['permalink'] != '' && $headers[C::KEY_AUTH] != '' && $post['comment_text'] != ''){
+            try{
+                $dotenv = Dotenv::createImmutable(__DIR__."/../../../../");
+                $dotenv->load();
+                $data = [
+                    'token_key' => $headers[C::KEY_AUTH],
+                    'comment_text' => $post['comment_text'],
+                    'permalink' => $post['permalink']
+                ];
+                $addController = new AddController($data);
+                $addCommentView = new AddView($addController);
+                if($addCommentView->isDone()) $response[C::KEY_DONE] = true;
+                else $response[C::KEY_MESSAGE] = $addCommentView->getMessage();
+                $response[C::KEY_CODE] = $addCommentView->getResponseCode();
+            }catch(Exception $e){
+                $response[C::KEY_CODE] = 500;
+                $msg = $e->getMessage();
+                switch($msg){
+                    case Ace::NOARTICLEPERMALINK_EXC:
+                    case Ace::NOCOMMENT_EXC:
+                    case Ace::NOTOKENKEY_EXC:
+                    case Ave::NOADDCOMMENTCONTROLLERINSTANCE_EXC:
+                    default:     
+                        $response[C::KEY_MESSAGE] = C::COMMENTCREATION_ERROR;
+                        break;
+                }
+            }
+        }
+        else{
+            $response[C::KEY_CODE] = 400;
+            $response[C::KEY_MESSAGE] = C::INSERTCOMMENT_ERROR;
+        }
         return $response;
     }
-
 }
 
 ?>
