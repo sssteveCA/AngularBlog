838e68fdd26ff67aea41bbad4312b4584762bb60
= <=>
2024-01-21 17:03:50 +0000

    Added responses errormessage.php

---
+++ src/assets/php/responses/errormessage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AngularBlog\Responses;
+
+use AngularBlog\Interfaces\Constants as C;
+
+/**
+ * JSON response with custom error message
+ */
+class ErrorMessage{
+
+    public static function content(array $params): array{
+        $response = [ C::KEY_CODE => 400, C::KEY_MESSAGE => "" ];
+        $isset = isset($params[C::KEY_CODE],$params[C::KEY_MESSAGE]);
+        $statusCode = (filter_var($statusCode, FILTER_VALIDATE_INT, array('options' => array('min_range' => 100, 'max_range' => 599))) !== false);
+        $notEmpty = ($params[C::KEY_MESSAGE] != '');
+        if($isset && $statusCode && $notEmpty)
+            $response = [ C::KEY_CODE => $params[C::KEY_CODE], C::KEY_MESSAGE => $params[C::KEY_MESSAGE]];
+        return $response;
+    }
+}
+
+?>
