d68ab4753dde6e9259e181ef6774c0a06afffc23
= <=>
2024-01-28 23:34:55 +0000

    Edit responses deletecomment.php content method

--- src/assets/php/responses/deletecomment.php
+++ src/assets/php/responses/deletecomment.php
@@ -19,6 +19,33 @@ class DeleteComment{
         $response = [
             C::KEY_CODE => 200, C::KEY_DONE => false, C::KEY_EXPIRED => false, C::KEY_MESSAGE => ''
         ];
+        if(isset($headers[C::KEY_AUTH],$delete['comment_id']) && $headers[C::KEY_AUTH] != '' && $delete['comment_id'] != ''){
+            try{
+                $dotenv = Dotenv::createImmutable(__DIR__."/../../../../");
+                $dotenv->load();
+                $token_data = ['token_key' => $headers[C::KEY_AUTH]];
+                $comment_data = ['id' => $delete['comment_id']];
+                $token = new Token($token_data);
+                $comment = new Comment($comment_data);
+                $dc_data = [
+                    'comment' => $comment,'token' => $token
+                ];
+                $deleteController = new DeleteController($dc_data);
+                $deleteView = new DeleteView($deleteController);
+                $response[C::KEY_MESSAGE] = $deleteView->getMessage();
+                if($deleteView->isDone()) $response[C::KEY_DONE] = true;
+                $errnoT = $deleteController->getToken()->getErrno();
+                if($errnoT == Te::TOKENEXPIRED) $response[C::KEY_EXPIRED] = true;
+                $response[C::KEY_CODE] = $deleteView->getResponseCode();
+            }catch(Exception $e){
+                $response[C::KEY_CODE] = 500;
+                $response[C::KEY_MESSAGE] = C::COMMENTDELETE_ERROR;
+            }
+        }
+        else{
+            $response[C::KEY_CODE] = 400;
+            $response[C::KEY_MESSAGE] = C::COMMENTDELETE_ERROR;
+        }
         return $response;
     }
 }
