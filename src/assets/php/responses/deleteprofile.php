510d10065c9031446a46a772bf1fefc90ce691fc
= <=>
2024-02-04 00:39:44 +0000

    Updated responses deleteprofile.php content method

--- src/assets/php/responses/deleteprofile.php
+++ src/assets/php/responses/deleteprofile.php
@@ -22,6 +22,45 @@ class DeleteProfile{
         ];
         $post = $params['post'];
         $headers = $params['headers'];
+        if(isset($headers[C::KEY_AUTH],$post["password"],$post["conf_password"])){
+            if($post["password"] == $post["conf_password"]){
+                $token_data = [ "token_key" => $headers[C::KEY_AUTH]];
+                try{
+                    $dotenv = Dotenv::createImmutable(__DIR__."/../../../../../../");
+                    $dotenv->load();
+                    $token = new Token($token_data);
+                    $user = new User();
+                    $dac_data = [
+                        "conf_password" => $post["conf_password"],
+                        "password" => $post["password"],
+                        "token" => $token, "user" => $user
+                    ];
+                    $dacController = new DeleteAccountController($dac_data);
+                    $dacView = new DeleteAccountView($dacController);
+                    $response[C::KEY_MESSAGE] = $dacView->getMessage();
+                    if($dacView->isDone()) $response[C::KEY_DONE] = true;
+                    else{
+                        $errnoT = $dacController->getToken()->getErrno();
+                        if($errnoT == Te::TOKENEXPIRED){
+                            $response[C::KEY_EXPIRED] = true;
+                            $response[C::KEY_MESSAGE] = Te::TOKENEXPIRED_MSG;
+                        }
+                    }
+                    $response[C::KEY_CODE] = $dacView->getResponseCode();
+                }catch(Exception $e){
+                    $response[C::KEY_CODE] = 500;
+                    $response[C::KEY_MESSAGE] = C::ACCOUNTDELETE_ERROR;
+                }
+            }
+            else{
+                $response[C::KEY_CODE] = 400;
+                $response[C::KEY_MESSAGE] = C::ERROR_CONFIRM_PASSWORD_DIFFERENT;
+            }
+        }
+        else{
+            $response[C::KEY_CODE] = 400;
+            $response[C::KEY_MESSAGE] = C::FILL_ALL_FIELDS;
+        }
         return $response;
     }
 
